6e859a99495ea497a6eff1b5ddfe49adf43912e8
giuratofabrizio <giurato.fabrizio@iisgalvanimi.edu.it>
2022-11-08 07:56:55 +0000

    punto 3

--- app3.py
+++ app3.py
@@ -1,20 +1,29 @@
 from flask import Flask, render_template, request, Response
 app = Flask(__name__)
 
-import pandas as pd
+import io
+import pandas as pd 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib
+matplotlib.use('Agg')
 import pymssql
+
+conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user='giurato.fabrizio', password='xxx123##', database='giurato.fabrizio')
 
 @app.route("/bestCustomers", methods=["GET"])
 def bestCustomers():
-    conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user='giurato.fabrizio', password='xxx123##', database='giurato.fabrizio')
-    query = f"SELECT TOP 10 cs.first_name, cs.last_name, cs.customer_id FROM sales.customers AS cs INNER JOIN sales.orders AS od ON cs.customer_id = od.customer_id"
-    df= pd.read_sql(query, conn)
+    query = 'SELECT TOP 10 customers.customer_id, customers.first_name, customers.last_name ,SUM(list_price *quantity) as spesa_totale FROM sales.customers INNER JOIN sales.orders ON sales.customers.customer_id = sales.orders.customer_id INNER JOIN sales.order_items ON sales.order_items.order_id = sales.orders.order_id GROUP BY customers.customer_id, customers.first_name, customers.last_name ORDER BY SUM(list_price * quantity) DESC '
+    tabella = pd.read_sql(query,conn)
+    print(tabella.columns.values)
+    return render_template("bestCustomers.html", nomiColonne = tabella.columns.values, dati = tabella.values)
 
-    if df.values.tolist() == []:
-        return render_template("error.html")
-    else:
-        return render_template("bestCustomers.html")
-
+@app.route("/totaleOrdini/<valore>")
+def totaleOrdini(valore):
+    query = f'SELECT * FROM sales.customers INNER JOIN sales.orders ON sales.customers.customer_id = sales.orders.customer_id WHERE customers.customer_id = {valore}'
+    tabella = pd.read_sql(query,conn)
+    return render_template("totaleOrdini.html", nomiColonne = tabella.columns.values, dati = tabella.values)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
